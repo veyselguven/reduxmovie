99d549a047306bb82d9c58176b1b8547aa964368
veysel basbaydar <vgbasbaydar@gmail.com>
2022-12-09 08:35:19 -0800

    header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { Segment, Container, List, Image } from "semantic-ui-react";
 function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <Segment
+        inverted
+        style={{ margin: "5em 0em 0em", padding: "2em 0em" }}
+        vertical
+      >
+        <Container textAlign="center">
+          <Image
+            src="https://react.semantic-ui.com/logo.png "
+            centered
+            size="mini"
+          />
+          <List horizontal inverted divided link size="small">
+            <List.Item as="a" href="#">
+              Site Map
+            </List.Item>
+            <List.Item as="a" href="#">
+              Contact Us
+            </List.Item>
+            <List.Item as="a" href="#">
+              Terms and Conditions
+            </List.Item>
+            <List.Item as="a" href="#">
+              Privacy Policy
+            </List.Item>
+          </List>
+        </Container>
+      </Segment>
+    </div>
+  );
 }
 
 export default Footer;
